10011427542c8e69b39d3057e6ad07fc30060fe5
jpjoseph12 <jpjoseph12@gmail.com>
2018-09-06 11:22:18 +0100

    player on keydown

---
+++ js/app.js
@@ -0,0 +1,11 @@
+function player(){
+  var x = window.getElementsByid("canvas.width")/2 
+  if (windows.event.keyCode == 37){
+    x--;
+  };
+  if (windows.event.keyCode == 39){
+    x++;
+  }
+
+}
+document.getElementById("")
